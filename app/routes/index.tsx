eb716b32d584eae16172bdcbbfaac1cf39926f72
hotdog7778 <rlaxorbs159@gmail.com>
2024-05-28 23:32:01 +0900

    links 모듈

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,41 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import Stylesheet1 from "~/styles/test.css"; // '~' 표시는 app 폴더를 지칭한다.
+
+export const links: LinksFunction = () => {
+  return [
+    // 파비콘을 추가하는 코드
+    {
+      rel: "icon",
+      href: "/favicon.png",
+      type: "image/png",
+    },
+
+    // => <link rel="icon" href="/favicon.png" type="image/png"> 로 렌더링
+
+    // 외부 스타일시트를 추가하는 코드
+    {
+      rel: "stylesheet",
+      href: "https://example-css-six.vercel.app/example.css",
+      crossOrigin: "true",
+    },
+
+    {
+      // 프리패치
+      rel: "prefetch",
+      as: "image",
+      href: "/img/cat.png",
+    },
+  ];
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Index</h1>
+      <p>Some content</p>
+      <input type="text" />
+      안녕하세요
     </div>
   );
 }
